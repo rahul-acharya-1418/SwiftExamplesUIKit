39a5f1fc0012476ef48bfd3d25f6760c04893abc
Rahul Acharya <acharyar00071@gmail.com>
2026-03-12 12:35:18 +0530

    init project setup

---
+++ SwiftExamplesUIKit/Controllers/SEHomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SEHomeViewController.swift
+//  SwiftExamplesUIKit
+//
+//  Created by Rahul Acharya on 12/03/26.
+//  Copyright © 2026 Swift Examples. All rights reserved.
+//
+
+import UIKit
+
+class SEHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
